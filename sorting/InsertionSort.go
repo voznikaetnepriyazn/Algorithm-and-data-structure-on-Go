a2575f0b431ed1076117f32e631a616c39752993
Daria Maslova <maslovadaria16.07@gmail.com>
2026-01-20 21:52:53 +0300

    insertion sort

--- sorting/InsertionSort.go
+++ sorting/InsertionSort.go
@@ -1,15 +1,10 @@
 package algs
-import (
-	"fmt"
-	"strconv"
-	"strings"
-	"math"
-)
-func insertionSort(arr []int{}){//O(n)
-	for i = 1, i < len(arr), i++{
-		j=i
-		for j >0{
-			if arr[j-1] > arr[j]{
+
+func insertionSort(arr []int) { //O(n)
+	for i := 1; i < len(arr); i++ {
+		j := i
+		for j > 0 {
+			if arr[j-1] > arr[j] {
 				arr[j-1], arr[j] = arr[j], arr[j-1]
 			}
 			j--
