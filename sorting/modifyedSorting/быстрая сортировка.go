3b7c3c3ab590da84fe3a0c028cb78d10f8a17f72
Daria Maslova <maslovadaria16.07@gmail.com>
2026-01-21 17:25:54 +0300

    quick sort

--- sorting/modifyedSorting/быстрая сортировка.go
+++ sorting/modifyedSorting/быстрая сортировка.go
@@ -1,43 +1,40 @@
 package algs
-import (
-	"fmt"
-	"sort"
-	"strings"
-	"math"
-)
-func qsortRecursive(arr []int{}){//O(n log(n))
-	if len(arr) <= 1{
+
+func qsortRecursive(arr []int) []int { //O(n log(n))
+	if len(arr) <= 1 {
 		return arr
 	}
 	pivot := median(arr)
 
 	i, j := 0, len(arr)-1
 
-	for i <= j{
-		for arr[i] < pivot{
+	for i <= j {
+		for arr[i] < pivot {
 			i++
 		}
-		for arr[j] > pivot{
+		for arr[j] > pivot {
 			j--
 		}
-		if i <= j{
+		if i <= j {
 			arr[i], arr[j] = arr[j], arr[i]
 			i++
 			j--
 		}
 	}
-	if j > 0{
+	if j > 0 {
 		qsortRecursive(arr[:j+1])
 	}
-	if i > len(arr){
+	if i > len(arr) {
 		qsortRecursive(arr[i:])
 	}
+
+	return arr
 }
 
-func median(arr []int){
-	tmp := make([]int(), len(arr)-1)
+func median(arr []int) int {
+	tmp := make([]int, len(arr)-1)
 	n := len(tmp)
-	if n%2 == 1{
+	if n%2 == 1 {
 		return tmp[n/2]
 	}
 	return (tmp[n/2-1] + tmp[n/2]) / 2
